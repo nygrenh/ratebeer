938b6c051b17791b831bf0e6fe5acecdd371d80d
Henrik Nygren <nygrenh@gmail.com>
2014-01-18 11:18:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/mluukkai/WebPalvelinohjelmointi2014/wiki/Web-palvelinohjelmointi-Ruby-on-Rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratebeer::Application.config.secret_key_base = '7602a49f1cabde7e52784484134d53e8415766127a1924b455db9c5ac0056657c671bfe003816df61d423a23e22d92a4ffc4cf61611c30b5aabc3a2b4bde94b7'
