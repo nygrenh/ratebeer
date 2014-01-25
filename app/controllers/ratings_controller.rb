a019d440d58275185fa562902b0f11996ce00fee
Henrik Nygren <nygrenh@gmail.com>
2014-01-25 17:35:47 +0200

    Viikko 2, tehtävä 7

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,14 @@
-class RatingsController < ApplicationController
+class RatingsController < ApplicationController	
   def index
   	@ratings = Rating.all
+  end
+
+  def new
+  	@rating = Rating.new
+  end
+
+  def create
+  	Rating.create params.require(:rating).permit(:score, :beer_id)
+  	redirect_to ratings_path
   end
 end
