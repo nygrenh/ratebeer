6c464f0835c342bd2d6ad45209c9c34911a9199e
Henrik Nygren <nygrenh@gmail.com>
2014-02-01 14:16:44 +0200

    Viikko 3, tehtävä 5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < ApplicationController
-    def new
-    end
+  def new
+  end
 
-    def create
-      user = User.find_by username: params[:username]
+  def create
+    user = User.find_by username: params[:username]
+    if user.nil?
+      redirect_to :back, notice: "User #{params[:username]} does not exist!"
+    else
       session[:user_id] = user.id unless user.nil?
-      redirect_to user   
+      redirect_to user, notice: "Welcome back!"
     end
+  end
 
-    def destroy
-      session[:user_id] = nil
-      redirect_to :root
-    end
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
+  end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,11 @@
 class Brewery < ActiveRecord::Base
 	include RatingAverage
 
+	validates :name, presence: true
+	validates :year, numericality: { only_integer: true, 
+									greater_than_or_equal_to: 1042, 
+									less_than_or_equal_to: 2014 }
+
 	has_many :beers, dependent: :destroy
 	has_many :ratings, through: :beers
 end
