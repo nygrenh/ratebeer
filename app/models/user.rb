da3b3aca58146f6888948a666424a745a608e219
Henrik Nygren <nygrenh@gmail.com>
2014-02-02 10:29:49 +0200

    Viikko 3, tehtävät 7-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
 	validates :username, uniqueness: true, length: { in: 3..15  }
 
 	has_many :ratings
+	has_many :beers, through: :ratings
+	has_many :memberships
+	has_many :beer_clubs, through: :memberships
 end
