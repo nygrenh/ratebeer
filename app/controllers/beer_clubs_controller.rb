5bfb32169d13caf93c7ca4c4959701f0058f5501
Henrik Nygren <nygrenh@gmail.com>
2014-02-14 13:20:39 +0200

    Viikko 5, tehtävät 7-8

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -11,6 +11,8 @@ class BeerClubsController < ApplicationController
   # GET /beer_clubs/1
   # GET /beer_clubs/1.json
   def show
+    @membership = Membership.new
+    @membership.beer_club = @beer_club
   end
 
   # GET /beer_clubs/new
@@ -72,4 +74,4 @@ class BeerClubsController < ApplicationController
     def beer_club_params
       params.require(:beer_club).permit(:name, :founded, :city)
     end
-end
+  end
--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,4 +1,9 @@
 class BeerClub < ActiveRecord::Base
 	has_many :memberships
 	has_many :members, through: :memberships, source: :user
+
+	def is_a_member(user_id)
+		membership = Membership.all.find_by beer_club_id:id, user_id:user_id
+		not membership.nil?
+	end
 end
