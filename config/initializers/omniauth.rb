585d9cf33e056a21b8ee42421fadc9173b709f77
Henrik Nygren <nygrenh@gmail.com>
2014-02-24 17:11:01 +0200

    An attempt to get this to build on Travis

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Settings.facebook_app_id, Settings.facebook_seceret
+	if Settings.table_exists?
+		provider :facebook, Settings.facebook_app_id, Settings.facebook_seceret
+	end
 end
 
