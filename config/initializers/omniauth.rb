cfabf6174946be2c9fab97257e16f2d979558f45
Henrik Nygren <nygrenh@gmail.com>
2014-02-24 19:21:09 +0200

    Siirrytty ympäristömuuttujien käyttöön omniauth.rb:ssä

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	if Settings.table_exists?
-		provider :facebook, Settings.facebook_app_id, Settings.facebook_seceret
-	end
+	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
 
